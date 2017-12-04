5c769ac49227c1c9e6367af55797f76f5f8f02a4
김병주 <superbottlegod@naver.com>
2017-12-05 00:11:41 +0900

    main.c

--- Rotation/main.c
+++ Rotation/main.c
@@ -1,39 +1,51 @@
 #include "header.h"
 
+void *trivial(char *str, int n, int d);
+void* jug(int d, int num, char *vector, int gcd);
+void Rev(int Length, int D, char* Str);
+void BS(int Length, int D, char* Str);
+int gcd(int num, int d);
+
 int main()
 {
-	char *vector,*rotate;
-	int length;
-	int rot;
-	int G = 0;
-	time_t start = 0, end = 0;
-	float t.trivial = 0, t.juggling = 0, t.blockswap = 0, t.reverse = 0;
-
-	printf("vector 의 길이를 입력하세요");
-	scanf("%d", &length);
-	printf("몇번 rotate?");
-	scanf("%d", &rot);
-	G = gcd(length,rot);
-
-	vector = (char *)malloc(sizeof(char)*length);
-	rotate = (char *)malloc(sizeof(char)*length);
-
+	clock_t start1, end1, start2, end2, start3, end3, start4, end4;
+	char *vector, *rotate;
+	int length, rot, G = 0;
+	printf("Enter the String distance and Rotate distance : ");
+	scanf("%d %d", &length, &rot);
+	G = gcd(length, rot);
+	vector = (char *)malloc(sizeof(char)*(length * 2));
+	rotate = (char *)malloc(sizeof(char)*(length * 2));
+	printf("Enter the String : ");
+	scanf("%s", vector);
 	for (int i = 0; i < length; i++)
 	{
-		scanf("%s", &vector[i]);
+		// scanf("%c", &vector[i]); //
 		rotate[i] = vector[i];
 	}
-	start = clock();
+	start1 = clock();
+	printf("Trivial Solution : ");
+	trivial(vector, length, rot);
+	end1 = clock();
+	
+	start2 = clock();
+	printf("Reverse : ");
+	Rev(length, rot, vector);
+	end2 = clock();
+
+	start3 = clock();
+	printf("Jugging : ");
 	jug(rot, length, rotate, G);
-	end = clock();
-	t.juggling = (float)(end - start);
+	end3 = clock();
 
-	strcpy(rotate, vector);
-	start = clock();
-	BS(length,rot,rotate);
-	end = clock();
-	t.blockswap = (float)(end - start);
+	start4 = clock();
+	printf("Block - Swap : ");
+	BS(length, rot, vector);
+	end4 = clock();
 
-	printf("\n[시간 측정]\nTrivial : %f초\nJuggling : %f초\nBlock-Swap : %f초\nReverse : %f초", t.trivial, t.juggling, t.blockswap, t.reverse);
+	printf("Trivial Solution Running Time : %fms", (double)(end1 - start1));
+	printf("\nReverse함수 Running Time : %fms", (double)(end2 - start2));
+	printf("\nJugging함수 Running Time : %fms\n", (double)(end3 - start3));
+	printf("Block - Swap Running Time : %fms", (double)(end4 - start4)); 
 	return 0;
 }
