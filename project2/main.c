855afc2c24c63121e0bafd92942563a7da5d19dc
dirgsmm <dbtmdgns4296@naver.com>
2017-12-04 22:34:35 +0900

    main jug 수정

--- project2/main.c
+++ project2/main.c
@@ -1,9 +1,9 @@
 #include "header.h"
-void* jug(int d, int num, int vector, int gcd);
+void* jug(int d, int num, char *vector, int gcd);
 int gcd(int num, int d);
 int main()
 {
-	int *vector,*rotate;
+	char *vector,*rotate;
 	int length;
 	int rot;
 	int G = 0;
@@ -12,11 +12,11 @@ int main()
 	printf("¸î¹ø rotate?");
 	scanf("%d", &rot);
 	G = gcd(length,rot);
-	vector = (int *)malloc(sizeof(int)*length);
-	rotate = (int *)malloc(sizeof(int)*length);
+	vector = (char *)malloc(sizeof(char)*length);
+	rotate = (char *)malloc(sizeof(char)*length);
 	for (int i = 0; i < length; i++)
 	{
-		scanf("%d", &vector[i]);
+		scanf("%s", &vector[i]);
 		rotate[i] = vector[i];
 	}
 	jug(rot, length, rotate, G);
