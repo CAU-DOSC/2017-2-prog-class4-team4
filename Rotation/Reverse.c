e8ce2a4cdfc12128f080dedf5803edfe29ddf379
김병주 <superbottlegod@naver.com>
2017-12-05 00:11:08 +0900

    put printf

--- Rotation/Reverse.c
+++ Rotation/Reverse.c
@@ -3,8 +3,23 @@
 void Rev(int Length, int D, char* Str)   //Reverse하는 함수
 {
 	reverse(0, D - 1, Str);
+	for (int i = 0; i < Length; i++)
+	{
+		printf("%c", Str[i]);
+	}
+	printf(" ");
 	reverse(D, Length - 1, Str);
+	for (int i = 0; i < Length; i++)
+	{
+		printf("%c", Str[i]);
+	}
+	printf(" ");
 	reverse(0, Length - 1, Str);
+	for (int i = 0; i < Length; i++)
+	{
+		printf("%c", Str[i]);
+	}
+	printf("\n");
 }
 void reverse(int start, int end, char* Str) //Reverse하는 함수에 포함되어 있는 함수
 {
