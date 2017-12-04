752c674a2a8f18ff695fe26687bdb79d2a5462ae
dirgsmm <dbtmdgns4296@naver.com>
2017-12-04 22:13:14 +0900

    main 추가 jug.c 추가

--- header.h
+++ header.h
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <time.h>
 
 void BS(int Length, int D, char* Str);
--- main.c
+++ main.c
@@ -1,6 +1,24 @@
-#include"header.h"
-
-int main(void)
+#include "header.h"
+void* jug(int d, int num, int vector, int gcd);
+int gcd(int num, int d);
+int main()
 {
-	
+	int *vector,*rotate;
+	int length;
+	int rot;
+	int G = 0;
+	printf("vector 의 길이를 입력하세요");
+	scanf("%d", &length);
+	printf("몇번 rotate?");
+	scanf("%d", &rot);
+	G = gcd(length,rot);
+	vector = (int *)malloc(sizeof(int)*length);
+	rotate = (int *)malloc(sizeof(int)*length);
+	for (int i = 0; i < length; i++)
+	{
+		scanf("%d", &vector[i]);
+		rotate[i] = vector[i];
+	}
+	jug(rot, length, rotate, G);
+	return 0;
 }
